57507dc6d00b78687902518fdf4005b9d4231a41
Niklas Persson <equadon@gmail.com>
2017-04-23 17:50:37 +0200

    Added very basic brace matcher and commenter.

---
+++ src/com/equadon/intellij/mips/MipsBraceMatcher.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017 Niklas Persson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.equadon.intellij.mips;
+
+import com.equadon.intellij.mips.lang.psi.MipsElementTypes;
+import com.equadon.intellij.mips.lang.psi.MipsTokenTypes;
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class MipsBraceMatcher implements PairedBraceMatcher {
+  private static final BracePair[] PAIRS = new BracePair[] {
+    new BracePair(MipsElementTypes.LPAREN, MipsElementTypes.RPAREN, false),
+    new BracePair(MipsElementTypes.LQUOTE, MipsElementTypes.RQUOTE, false)
+  };
+
+  @Override
+  public BracePair[] getPairs() {
+    return PAIRS;
+  }
+
+  @Override
+  public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType lBraceType, @Nullable IElementType type) {
+    return MipsTokenTypes.WHITE_SPACES.contains(type) ||
+        MipsTokenTypes.COMMENTS.contains(type) ||
+        type == MipsElementTypes.RPAREN ||
+        type == MipsElementTypes.RQUOTE;
+  }
+
+  @Override
+  public int getCodeConstructStart(PsiFile file, int openingBraceOffset) {
+    return openingBraceOffset;
+  }
+}
